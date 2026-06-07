b958b80561f8728e035bad26b6272cdd50e2b55c
Izzy Beers <izzybeers@Izzys-MacBook-Pro.local>
2026-06-06 22:42:30 -0400

    fix bug in EditBookInfo, update styling on head to head bar plot, and identify favorite author in head to head section

--- front_end/src/components/BarChart.js
+++ front_end/src/components/BarChart.js
@@ -2,15 +2,10 @@ import React from 'react'
 
 const BarChart = ({value, color}) => {
     const height = value*10
-    console.log('entered bar chart')
-    console.log(height)
-    console.log(value)
-    console.log(color)
   return (
     <div
     className = {`w-16 ${color}`}
     style = {{height: `${height}%`}}>
-    
     </div>
   )
 }
