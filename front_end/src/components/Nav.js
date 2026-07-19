bd1620843cf38187649213add7b212cd830a104e
Izzy Beers <izzybeers@Izzys-MacBook-Pro.local>
2026-07-18 22:49:18 -0400

    update nav styling

--- front_end/src/components/Nav.js
+++ front_end/src/components/Nav.js
@@ -12,17 +12,17 @@ const Nav = () => {
     console.log(membersData?.length)
     const selectedUser = useSelector((state) => state.members.selectedMemberID)
     return (
-        <div className = 'flex flex-wrap w-full h-[75px] sm:h-[50px] bg-blue-100 border-black-100 text-md md:text-lg text-blue-900 font-medium justify-center items-center'>
-            <div className = 'px-5 cursor-pointer ' onClick={() => navigate('/')}>
+        <div className = 'flex flex-nowrap whitespace-nowrap w-full h-[75px] sm:h-[50px] bg-blue-100 border-black-100 text-md md:text-lg text-blue-900 font-medium justify-center items-center'>
+            <div className = 'px-2 sm:px-5 cursor-pointer ' onClick={() => navigate('/')}>
                 <p>Home</p>
             </div>
-            <div className = ' px-5 cursor-pointer ' onClick={() => navigate('/friend-comparisons')}>
+            <div className = ' px-2 sm:px-5 cursor-pointer ' onClick={() => navigate('/friend-comparisons')}>
                 <p>Analysis</p>
             </div>
-            <div className = 'px-5 cursor-pointer 'onClick={() => navigate('/add-new')}>
+            <div className = 'px-2 px-5 cursor-pointer 'onClick={() => navigate('/add-new')}>
                 <p>Add New</p>
             </div>
-            <div className = 'px-5 cursor-pointer ' onClick={() => navigate('/raw-data')}>
+            <div className = 'px-2 px-5 cursor-pointer ' onClick={() => navigate('/raw-data')}>
                 <p>Raw Data</p>
             </div>
             <div className = 'flex pr-5 sm:ml-auto text-xl mx-auto sm:mx-0 font-bold text-black-100'>
